e4e097db556ededffd5a6efbae3b0541f6936f19
jack <144657795+fieje@users.noreply.github.com>
2024-05-09 16:20:33 +0300

    Add files via upload

---
+++ 12.12/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Price
+{
+    public string ProductName { get; set; }
+    public string StoreName { get; set; }
+    public double Cost { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Price> priceList = new List<Price>();
+
+        AddData(priceList);
+
+        priceList = SortByStoreName(priceList);
+
+        Console.WriteLine("Enter the product name to search:");
+        string productName = Console.ReadLine();
+        DisplayProductInfo(priceList, productName);
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    static void AddData(List<Price> priceList)
+    {
+        Console.WriteLine("Enter the number of prices to add:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Price price = new Price();
+            Console.WriteLine("Enter product name:");
+            price.ProductName = Console.ReadLine();
+
+            Console.WriteLine("Enter store name:");
+            price.StoreName = Console.ReadLine();
+
+            Console.WriteLine("Enter product cost:");
+            price.Cost = Convert.ToDouble(Console.ReadLine());
+
+            priceList.Add(price);
+        }
+    }
+
+    static List<Price> SortByStoreName(List<Price> priceList)
+    {
+        return priceList.OrderBy(x => x.StoreName).ToList();
+    }
+
+    static void DisplayProductInfo(List<Price> priceList, string productName)
+    {
+        bool found = false;
+        foreach (var price in priceList)
+        {
+            if (price.ProductName == productName)
+            {
+                Console.WriteLine($"Product: {price.ProductName}, Store: {price.StoreName}, Cost: {price.Cost} UAH");
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine("Product not found.");
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddData()
+        {
+            List<Price> priceList = new List<Price>();
+
+            AddData(priceList);
+
+            Assert.AreEqual(2, priceList.Count); 
+        }
+
+        [TestMethod]
+        public void TestSortByStoreName()
+        {
+            List<Price> priceList = new List<Price>
+            {
+                new Price { ProductName = "Product1", StoreName = "StoreB", Cost = 10.99 },
+                new Price { ProductName = "Product2", StoreName = "StoreA", Cost = 20.99 }
+            };
+
+            priceList = SortByStoreName(priceList);
+
+            Assert.AreEqual("StoreA", priceList[0].StoreName);
+            Assert.AreEqual("StoreB", priceList[1].StoreName);
+        }
+
+        [TestMethod]
+        public void TestDisplayProductInfo_ProductNotFound()
+        {
+            List<Price> priceList = new List<Price>
+            {
+                new Price { ProductName = "Product1", StoreName = "StoreA", Cost = 10.99 },
+                new Price { ProductName = "Product2", StoreName = "StoreB", Cost = 20.99 }
+            };
+            string productName = "Product3";
+
+            var consoleOutput = GetConsoleOutput(() => DisplayProductInfo(priceList, productName));
+
+            Assert.IsTrue(consoleOutput.Contains("Product not found."));
+        }
+
+        private string GetConsoleOutput(Action action)
+        {
+            using (StringWriter consoleOutputWriter = new StringWriter())
+            {
+                Console.SetOut(consoleOutputWriter);
+                action.Invoke();
+                return consoleOutputWriter.ToString();
+            }
+        }
+
+        private void AddData(List<Price> priceList)
+        {
+            Console.WriteLine("Adding test data...");
+            priceList.Add(new Price { ProductName = "Product1", StoreName = "StoreA", Cost = 10.99 });
+            priceList.Add(new Price { ProductName = "Product2", StoreName = "StoreB", Cost = 20.99 });
+        }
+
+        private List<Price> SortByStoreName(List<Price> priceList)
+        {
+            Console.WriteLine("Sorting by store name...");
+            return priceList.OrderBy(x => x.StoreName).ToList();
+        }
+
+        private void DisplayProductInfo(List<Price> priceList, string productName)
+        {
+            Console.WriteLine($"Searching for product: {productName}...");
+            var product = priceList.FirstOrDefault(p => p.ProductName == productName);
+            if (product != null)
+            {
+                Console.WriteLine($"Product: {product.ProductName}, Store: {product.StoreName}, Cost: {product.Cost}");
+            }
+            else
+            {
+                Console.WriteLine("Product not found.");
+            }
+        }
+    }
+}
